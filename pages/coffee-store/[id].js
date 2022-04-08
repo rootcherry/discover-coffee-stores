cda27a09f41dae16da820d59dfcc1dc01558b287
Chris N <hi.chrisn@gmail.com>
2022-04-08 14:41:34 -0300

    add routes to dynamic pages with link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
-  console.log('router', router);
+  // console.log('router', router);
   return (
     <div>
       Coffee Store Page {router.query.id}
       <Link href="/">
         <a>Back to home</a>
+      </Link>
+      <Link href="/coffee-store/dynamic">
+        <a>Go to page dynamic</a>
       </Link>
     </div>
   );
