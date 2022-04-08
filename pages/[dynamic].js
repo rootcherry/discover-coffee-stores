d2597fc029ac77acd7d64d60be9452ea143e0454
Chris N <hi.chrisn@gmail.com>
2022-04-08 15:26:16 -0300

    exercise: routing

---
+++ pages/[dynamic].js
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+const DynamicRoute = () => {
+  const router = useRouter();
+  const query = router.query.dynamic;
+  return (
+    <div>
+      <Head>
+        <title>{query}</title>
+      </Head>
+      Page {query}
+    </div>
+  );
+};
+
+export default DynamicRoute;
---
+++ pages/courses/nextjs.js
@@ -0,0 +1,7 @@
+const NextJs = () => {
+  return (
+    <div>Welcome to Next.js with Ankita</div>
+  );
+};
+
+export default NextJs;
