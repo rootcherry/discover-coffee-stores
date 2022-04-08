e35ffbe2ac87e0f4ed040aa74c424d5fd92bd8f5
Chris N <hi.chrisn@gmail.com>
2022-04-08 16:57:37 -0300

    hero image on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-import Banner from '../components/banner'
+import Banner from '../components/banner';
 
 export default function Home() {
 
@@ -18,7 +19,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
+        <Banner 
+          buttonText="View stores nearby" 
+          handleOnClick={handleOnBannerBtnClick} 
+        />
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
       </main>
     </div>
   )
