a1daf7b5fb10c6d2f3a310c3906df8b270c4afc2
Chris N <hi.chrisn@gmail.com>
2022-04-08 14:37:54 -0300

    add route using link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log('router', router);
   return (
-    <div>Coffee Store Page {router.query.id}</div>
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </div>
   );
 };
 
