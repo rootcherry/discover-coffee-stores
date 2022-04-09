683c593046a93000dc0220cd0046ac2ba5b5f6a5
Chris N <hi.chrisn@gmail.com>
2022-04-09 16:01:38 -0300

    refactor coffee store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from 'next/head';
 
 import coffeeStoresData from '../../data/coffee-stores.json';
 
 export function getStaticProps(staticProps) { // {params}
   const params = staticProps.params;
-  console.log('params', params);
+  // console.log('params', params);
   return {
     props: {
       coffeeStore: coffeeStoresData.find(coffeeStore => {
@@ -16,11 +17,16 @@ export function getStaticProps(staticProps) { // {params}
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoresData.map(coffeeStore => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+
   return {
-    paths: [
-      { params: { id: '0' } },
-      { params: { id: '1' } }
-    ],
+    paths,
     fallback: true,
   };
 }
@@ -29,21 +35,24 @@ const CoffeeStore = (props) => {
   const router = useRouter();
   // console.log('router', router);
   // console.log('props', props);
+
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
+  const { address, name, neighbourhood } = props.coffeeStore;
+
   return (
     <div>
-      Coffee Store Page {router.query.id}
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a>Back to home</a>
       </Link>
-      <Link href="/coffee-store/dynamic">
-        <a>Go to page dynamic</a>
-      </Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
