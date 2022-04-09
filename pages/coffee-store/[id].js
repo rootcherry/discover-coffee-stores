a8a338fa20575e90e3bcd4a2fd98ad6b5dce1535
Chris N <hi.chrisn@gmail.com>
2022-04-09 16:46:54 -0300

    coffee store page styling

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Head from 'next/head';
+import Image from 'next/image';
 
 import coffeeStoresData from '../../data/coffee-stores.json';
 
+import styles from '../../styles/coffee-store.module.css';
+
 export function getStaticProps(staticProps) { // {params}
   const params = staticProps.params;
-  // console.log('params', params);
+
   return {
     props: {
       coffeeStore: coffeeStoresData.find(coffeeStore => {
@@ -33,26 +36,41 @@ export function getStaticPaths() {
 
 const CoffeeStore = (props) => {
   const router = useRouter();
-  // console.log('router', router);
-  // console.log('props', props);
-
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighbourhood } = props.coffeeStore;
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
 
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image 
+            src={imgUrl} 
+            width={600} 
+            height={360} 
+            className={styles.storeImg} 
+            alt={name}
+          ></Image>
+        </div>
+
+        <div className={styles.col2}>
+          <p>{address}</p>
+          <p>{neighbourhood}</p>
+        </div>
+      </div>
     </div>
   );
 };
