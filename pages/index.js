246323f3cbc6877adcc5fb7142b1ea3c896926b9
Chris N <hi.chrisn@gmail.com>
2022-04-10 17:30:44 -0300

    refactor coffee store app

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { fetchCoffeeStores } from '../lib/coffee-store';
 
 import useTrackLocation from '../hooks/use-track-location';
 import { useEffect, useState, useContext } from "react";
-import { ACTION_TYPES, StoreContext } from '../pages/_app';
+import { ACTION_TYPES, StoreContext } from '../store/store-context';
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores();
