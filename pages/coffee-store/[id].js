07a26b95167cf1f9445235ffe543093432ee418b
Chris N <hi.chrisn@gmail.com>
2022-04-10 14:53:45 -0300

    fallback in next.js

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -11,11 +11,12 @@ export async function getStaticProps(staticProps) { // {params}
   const params = staticProps.params;
 
   const coffeeStores = await fetchCoffeeStores();
+  const findCoffeeStoreById = coffeeStores.find(coffeeStore => {
+    return coffeeStore.id.toString() === params.id; // dynamic id
+  });
   return {
     props: {
-      coffeeStore: coffeeStores.find(coffeeStore => {
-        return coffeeStore.id.toString() === params.id; // dynamic id
-      }),
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
     },
   };
 }
