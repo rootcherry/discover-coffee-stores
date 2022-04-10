3280f42d64771fa35f901d62049d76f46f711dd5
Chris N <hi.chrisn@gmail.com>
2022-04-10 17:07:00 -0300

    coffee store page for non-ssg routes

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,3 +1,4 @@
+import { useContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Head from 'next/head';
@@ -6,6 +7,10 @@ import cls from 'classnames';
 
 import styles from '../../styles/coffee-store.module.css';
 import { fetchCoffeeStores } from "../../lib/coffee-store";
+
+import { StoreContext } from '../_app';
+
+import { isEmpty } from "../../utils";
 
 export async function getStaticProps(staticProps) { // {params}
   const params = staticProps.params;
@@ -37,13 +42,34 @@ export async function getStaticPaths() {
   };
 }
 
-const CoffeeStore = (props) => {
+const CoffeeStore = (initialProps) => {
   const router = useRouter();
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighborhood, imgUrl } = props.coffeeStore;
+  const id = router.query.id;
+
+  const [coffeeStore, setCoffeeStore] = useState(initialProps.coffeeStore);
+
+  const {
+    state: {
+      coffeeStores
+    }
+  } = useContext(StoreContext);
+
+  useEffect(() => {
+    if (isEmpty(initialProps.coffeeStore)) {
+      if (coffeeStores.length > 0) {
+        const findCoffeeStoreById = coffeeStores.find(coffeeStore => {
+          return coffeeStore.id.toString() === id; // dynamic id
+        });
+        setCoffeeStore(findCoffeeStoreById);
+      }
+    }
+  }, [id]);
+
+  const { address, name, neighborhood, imgUrl } = coffeeStore;
 
   const handleUpvoteButton = () => {
     console.log('handle upvote');
