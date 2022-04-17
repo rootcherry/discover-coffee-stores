1eeb12dcfa48e93507b41a5b31fd5f8b0e35c40b
Chris N <hi.chrisn@gmail.com>
2022-04-17 16:05:23 -0300

    refactor create coffee store api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,12 +1,6 @@
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
-
-const table = base('coffee-stores');
-
-// console.log({table});
+import { table, getMinifiedRecords } from '../../lib/airtable';
 
 const createCoffeeStore = async (req, res) => {
-  // console.log({req});
   if (req.method === 'POST') {
     // find a record
 
@@ -18,13 +12,9 @@ const createCoffeeStore = async (req, res) => {
           .select({
             filterByFormula: `id=${id}`
           }).firstPage();
-  
-        console.log({ findCoffeeStoreRecords });
-  
+
         if (findCoffeeStoreRecords.length !== 0) {
-          const records = findCoffeeStoreRecords.map((record) => {
-            return { ...record.fields };
-          });
+          const records = getMinifiedRecords(findCoffeeStoreRecords);
           res.json(records);
         } else {
           // create a record
@@ -43,10 +33,7 @@ const createCoffeeStore = async (req, res) => {
               },
             ]);
     
-            const records = createRecords.map((record) => {
-              return { ...record.fields };
-            });
-    
+            const records = getMinifiedRecords(createRecords);
             res.json(records);
           } else {
             res.status(400);
