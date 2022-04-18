b5d89c1b3e8261c8f1e0d8db97b3704d44bfc565
Chris N <hi.chrisn@gmail.com>
2022-04-18 15:42:15 -0300

    refactor coffee store by id api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,24 +1,19 @@
-import { table, getMinifiedRecords } from '../../lib/airtable';
+import { table, getMinifiedRecords, findRecordByFilter } from '../../lib/airtable';
 
 const createCoffeeStore = async (req, res) => {
-  if (req.method === 'POST') {
-    // find a record
+  if (req.method === "POST") {
+    //find a record
 
     const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
 
     try {
       if (id) {
-        const findCoffeeStoreRecords = await table
-          .select({
-            filterByFormula: `id="${id}"`
-          }).firstPage();
+        const records = await findRecordByFilter(id);
 
-        if (findCoffeeStoreRecords.length !== 0) {
-          const records = getMinifiedRecords(findCoffeeStoreRecords);
+        if (records.length !== 0) {
           res.json(records);
         } else {
-          // create a record
-  
+          //create a record
           if (name) {
             const createRecords = await table.create([
               {
@@ -32,22 +27,22 @@ const createCoffeeStore = async (req, res) => {
                 },
               },
             ]);
-    
+
             const records = getMinifiedRecords(createRecords);
             res.json(records);
           } else {
             res.status(400);
-            res.json({ message: 'Id or name is missing' });
+            res.json({ message: "Id or name is missing" });
           }
         }
       } else {
         res.status(400);
-        res.json({ message: 'Id is missing' });
+        res.json({ message: "Id is missing" });
       }
     } catch (err) {
-      console.error('Error creating or finding a store', err);
+      console.error("Error creating or finding a store", err);
       res.status(500);
-      res.json({ message: 'Error creating or finding a store', err });
+      res.json({ message: "Error creating or finding a store", err });
     }
   }
 };
