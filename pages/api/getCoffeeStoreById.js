efb28c2d27f50f3cd2ddb06438c7c65cf6e9ff5c
Chris N <hi.chrisn@gmail.com>
2022-04-18 15:07:52 -0300

    coffee store by id api

---
+++ pages/api/getCoffeeStoreById.js
@@ -0,0 +1,17 @@
+const getCoffeeStoreById = (req, res) => {
+  const {id} = req.query;
+
+  try {
+    if (id) {
+      res.json({ message: `id is created ${id}` });
+    } else {
+      res.status(400);
+      res.json({ message: 'id is missing' });
+    }
+  } catch (err) {
+    res.status(500);
+    res.json({ message: 'Something went wrong', err });
+  }
+};
+
+export default getCoffeeStoreById;
